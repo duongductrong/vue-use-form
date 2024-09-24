47305d65630e1005eb92b5ed712100e6b108c096
duongductrong <duongductrong06@gmail.com>
2024-09-24 23:08:24 +0700

    :art: chore: format code at createFieldArray

--- packages/core/src/logic/createFieldArray.ts
+++ packages/core/src/logic/createFieldArray.ts
@@ -1,57 +1,58 @@
 import { reactive } from 'vue'
 import set from 'lodash.setwith'
-import type { FieldArray } from '../types/fieldArray';
-import type { FieldArrayPath } from '../types/path/path';
-import type { FieldValues } from '../types/filed';
-
+import type { FieldArray } from '../types/fieldArray'
+import type { FieldArrayPath } from '../types/path/path'
+import type { FieldValues } from '../types/filed'
 
 // _options: UseFieldArrayProps<TFieldValues, TFieldArrayName>
 export function createFieldArray<
-TFieldValues extends FieldValues = FieldValues,
-TFieldArrayName extends FieldArrayPath<TFieldValues> = FieldArrayPath<TFieldValues>,
+  TFieldValues extends FieldValues = FieldValues,
+  TFieldArrayName extends FieldArrayPath<TFieldValues> = FieldArrayPath<TFieldValues>
 >() {
   // const { control, name } = _options
 
   const fields = reactive([])
 
   const updateField = (
-    value: 
-    Partial<FieldArray<TFieldValues, TFieldArrayName>>
-    | Partial<FieldArray<TFieldValues, TFieldArrayName>>[],
+    value:
+      | Partial<FieldArray<TFieldValues, TFieldArrayName>>
+      | Partial<FieldArray<TFieldValues, TFieldArrayName>>[],
     cb: (obj: any) => void
-    ) => {
-      if (!Array.isArray(value)) {
-        value = [value]
-      }
-  
-      value.forEach(v => {
-        const obj = Object.create(null)
-        Object.entries(v).forEach(([key, val]) => {
-          set(obj, key, val)
-        })
-        cb(obj)
-      })
+  ) => {
+    if (!Array.isArray(value)) {
+      value = [value]
     }
 
+    value.forEach((v) => {
+      const obj = Object.create(null)
+      Object.entries(v).forEach(([key, val]) => {
+        set(obj, key, val)
+      })
+      cb(obj)
+    })
+  }
+
   const append = (
-    value: 
-      Partial<FieldArray<TFieldValues, TFieldArrayName>>
-      | Partial<FieldArray<TFieldValues, TFieldArrayName>>[],
+    value:
+      | Partial<FieldArray<TFieldValues, TFieldArrayName>>
+      | Partial<FieldArray<TFieldValues, TFieldArrayName>>[]
   ) => {
     updateField(value, (obj) => fields.push(obj))
   }
 
   const prepend = (
-    value: 
-    |  Partial<FieldArray<TFieldValues, TFieldArrayName>>
-      | Partial<FieldArray<TFieldValues, TFieldArrayName>>[],
+    value:
+      | Partial<FieldArray<TFieldValues, TFieldArrayName>>
+      | Partial<FieldArray<TFieldValues, TFieldArrayName>>[]
   ) => {
     updateField(value, (obj) => fields.unshift(obj))
   }
 
   const insert = (
-    index: number, value: Partial<FieldArray<TFieldValues, TFieldArrayName>>
-    | Partial<FieldArray<TFieldValues, TFieldArrayName>>[],
+    index: number,
+    value:
+      | Partial<FieldArray<TFieldValues, TFieldArrayName>>
+      | Partial<FieldArray<TFieldValues, TFieldArrayName>>[]
   ) => {
     updateField(value, (obj) => fields.splice(index, 0, obj))
   }
@@ -71,7 +72,7 @@ TFieldArrayName extends FieldArrayPath<TFieldValues> = FieldArrayPath<TFieldValu
       index = [index]
     }
 
-    index.forEach(i => {
+    index.forEach((i) => {
       fields.splice(i, 1)
     })
   }
